dce0222d1350c54702f8207c05a548a90142f3a5
Rainer Hihn <hihn.rainer@gmail.com>
2022-05-08 18:40:35 +0200

    Added management command to delete empty weeks

---
+++ web/management/commands/del_empty_weeks.py
@@ -0,0 +1,17 @@
+import ipdb
+from django.core.mail import mail_admins
+from django.core.management import BaseCommand
+from django.db.models import Count
+
+from web.models import Week
+
+
+class Command(BaseCommand):
+    help = "Print search results"
+
+    def handle(self, *args, **options):
+        qs = Week.objects.annotate(number_of_entries=Count("entry")).filter(
+            note__exact="", number_of_entries=0
+        )
+        for item in qs:
+            item.delete()
