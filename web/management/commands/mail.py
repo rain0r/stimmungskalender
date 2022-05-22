b0b5dffacde1ce08b9389b59b76c65f20c5e00fb
Rainer Hihn <hihn.rainer@gmail.com>
2022-05-22 12:50:05 +0200

    Removed unused command

--- web/management/commands/mail.py
+++
@@ -1,9 +0,0 @@
-from django.core.mail import mail_admins
-from django.core.management import BaseCommand
-
-
-class Command(BaseCommand):
-    help = "Print search results"
-
-    def handle(self, *args, **options):
-        mail_admins("Subject here", "Here is the message.", fail_silently=False)
