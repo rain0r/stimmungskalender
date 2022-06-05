e0746ba6c16794f39ace29a253174596d2a8f3e1
Rainer Hihn <hihn.rainer@gmail.com>
2022-06-05 17:49:17 +0200

    Use tabs for graphs

--- web/js/src/sk.base.js
+++ web/js/src/sk.base.js
@@ -1,3 +1,11 @@
 import "popper.js/dist/popper.min.js";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 $('[data-toggle="tooltip"]').tooltip();
+
+$('a[data-toggle="tab"]').on("shown.bs.tab", function (event) {
+  window.location.hash = $(event.target).attr("id");
+});
+
+if (window.location.hash) {
+  $("#pie-tab").tab("show");
+}
