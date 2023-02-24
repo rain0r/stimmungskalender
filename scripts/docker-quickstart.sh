284b1bf44996a80f8dfeab24b249508448683a34
rain0r <hihn.rainer@gmail.com>
2023-02-24 23:04:01 +0100

    Update docker-quickstart.sh

--- scripts/docker-quickstart.sh
+++ scripts/docker-quickstart.sh
@@ -6,12 +6,15 @@ wget https://raw.githubusercontent.com/rain0r/stimmungskalender/master/docker/ap
 
 docker run --env-file sqlite-env \
         -v static:/srv/www/stimmungskalender/static \
-        -v dbdir:/db rain0r/stimmungskalender first_run
+        -v dbdir:/db rain0r/stimmungskalender first_run \
+        --name stimmungskalender
 
 docker run --env-file sqlite-env \
         -v static:/srv/www/stimmungskalender/static \
-        -v dbdir:/db rain0r/stimmungskalender default_user
+        -v dbdir:/db rain0r/stimmungskalender default_user \
+        --name stimmungskalender
 
 docker run -p 8080:8000 --env-file sqlite-env \
         -v static:/srv/www/stimmungskalender/static \
-        -v dbdir:/db rain0r/stimmungskalender
+        -v dbdir:/db rain0r/stimmungskalender \
+        --name stimmungskalender
