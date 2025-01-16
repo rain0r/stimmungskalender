24ba482a3ea2e423e3ec3e02f3fab21b8f7f13a8
Rainer Hihn <hihn.rainer@gmail.com>
2025-01-16 21:36:56 +0100

    Use config()

--- stimmungskalender/settings.py
+++ stimmungskalender/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-1@rhs^-co74$9l9!+0$@s2xm^57aoilo%c(h_33@02q3tn5wyh"
+SECRET_KEY = config("SECRET_KEY", default="PLEASE-CHANGE-ME")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG", default=False, cast=bool)
 
 ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="127.0.0.1", cast=Csv())
 
